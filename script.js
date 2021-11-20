23ba8ea2d2bbddb8a50f50de16572e8c521d0ca5
thejulianflores <github@thejulianflores.com>
2021-11-20 03:06:46 -0600

    Add basic operation functionality

---
+++ script.js
@@ -0,0 +1,47 @@
+function add(num1, num2){
+    return(num1+num2)
+}
+
+function subtract(num1, num2){
+    return(num1-num2)
+}
+
+function multiply(num1, num2){
+    return(num1*num2)
+}
+
+function divide(num1, num2){
+    if(num2 != 0){
+        return( num1 / num2)
+    }
+    else{
+        return('Cannot Divide By 0')
+    }
+}
+
+/**
+ * @param {*} operator - One of the four operators +, -, *, /
+ * @param {*} num1 - Any rational value
+ * @param {*} num2 - Any rational value
+ * @returns the result of num1 $ num2, where $ is the operator chosen
+ */
+
+function operate(operator, num1, num2){
+    let result
+    switch( operator ){
+        case '+':
+            result = add(num1, num2)
+            break;
+        case '-':
+            result = subtract(num1, num2)
+            break;
+        case '*':
+            result = multiply(num1, num2)
+            break;
+        case '/':
+            result = divide(num1, num2)
+            break;
+    }
+    return( result )
+}
+
